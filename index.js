3b6174b13693313b1e95620b0ab63767ad8e6f45
Diego Diniz <diegodinizz@gmail.com>
2021-09-20 23:46:08 +0100

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // We import the object from the data file. Inside that object there is a function to get players data
-const data = require("./data");
+const data = require('./data')
+
+const players = data.getPlayers()
 
 /**
  * Test 1
@@ -11,49 +13,98 @@ const data = require("./data");
  * PLAYER 2...
  */
 
- // Your code
+function printPlayers (players) {
+  for (let i = 0; i < players.length; i++) {
+    const player = players[i]
+    console.log(`PLAYER ${i + 1}`)
+    console.log(`NAME: ${player.name}`)
+    console.log(`LASTNAME: ${player.lastname}`)
+    console.log(`POSITION: ${player.position}`)
+  }
+}
 
-
+printPlayers(players)
 
 /**
  * Test 2
  * Write a function to log in the console an array with only the names of the players, ordered by name length descending
  */
 
-// Your code
+function printPlayersByNameLength (players) {
+  const playersByNameLength = players
+    .map(player => player.name)
+    .sort((a, b) => b.length - a.length)
+  console.log(playersByNameLength)
+}
 
-
-
-
+printPlayersByNameLength(players)
 
 /**
  * Test 3
  * Write a function to log in the console the average number of goals there will be in a match if all the players in the data play on it
  * scoringChance means how many goals per 100 matches the player will score
- * Example: 10 players play in a match, all of them has 0.11 scoringChance, the result will be 1.1 average goals 
+ * Example: 10 players play in a match, all of them has 0.11 scoringChance, the result will be 1.1 average goals
  * Output example -> Goals per match: 2.19
  */
 
-// Your code
+function printAverageGoalsPerMatch (players) {
+  const goalsPerMatch = players.reduce(
+    (acc, player) => acc + player.scoringChance / 100,
+    0
+  )
+  console.log(`Goals per match: ${goalsPerMatch.toFixed(2)}`)
+}
 
-
+printAverageGoalsPerMatch(players)
 
 /**
  * Test 4
  * Write a function that accepts a name, and logs the position of the player with that name (by position it means striker, goalkeeper...)
  */
 
-// Your code
+function getPlayerPosition (name) {
+  const player = players.find(player => player.name === name)
+  console.log(`${player.name}: ${player.position}`)
+}
 
-
+getPlayerPosition('Diego')
 
 /**
  * Test 5
  * Write a function that splits all the players randomly into 2 teams, Team A and Team B. Both teams should have same number of players.
  * The function should log the match score, using the average number of goals like the Test 3 and rounding to the closest integer
  * Example:
- *      Team A has 4 players, 2 with 50 scoringChance and 2 with 70 scoringChance. 
+ *      Team A has 4 players, 2 with 50 scoringChance and 2 with 70 scoringChance.
  *      The average score for the team would be 2.4 (50+50+70+70 / 100), and the closest integer is 2, so the Team A score is 2
  */
 
-// Your code
+function splitPlayersIntoTeams (players) {
+  // First shuffle the players
+  for (let i = players.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = players[i]
+    players[i] = players[j]
+    players[j] = temp
+  }
+
+  const teamShuffle = players
+
+  // Then split the players into 2 teams evenly
+  const teamA = teamShuffle.slice(0, Math.floor(teamShuffle.length / 2))
+  const teamB = teamShuffle.slice(Math.floor(teamShuffle.length / 2))
+
+  // Then calculate the average score for each team
+  const teamAScore = teamA.reduce(
+    (acc, player) => acc + player.scoringChance / 100,
+    0
+  )
+  const teamBScore = teamB.reduce(
+    (acc, player) => acc + player.scoringChance / 100,
+    0
+  )
+
+  console.log(`Team A: ${Math.round(teamAScore)}`)
+  console.log(`Team B: ${Math.round(teamBScore)}`)
+}
+
+splitPlayersIntoTeams(players)
--- reactTest.tsx
+++ reactTest.tsx
@@ -1,7 +1,7 @@
 /**
  * If you want to prove your React skills, please try this test.
- * 
- * TIPS: 
+ *
+ * TIPS:
  * - These are a theoretical questions, there is no need to install packages or run the code.
  */
 
@@ -9,38 +9,41 @@ import React from "react";
 
 /**
  * TEST 1
- * 
+ *
  * This test is a simple one with conditional rendering.
- * 
- * This component has to return:
+ *
+ * This typescript component has to return:
  *  The name in blue if the age is greater than or equal than 18.
  *  The name in red otherwise.
  */
 
 interface ITest1ComponentProps {
-    name: string;
-    age:number;
+  name: string;
+  age: number;
 }
 
 export const Test1Component = (props: ITest1ComponentProps) => {
-
-}
-
+  const { name, age } = props;
+  if (age >= 18) {
+    return <span style={{ color: "blue" }}>{name}</span>;
+  } else {
+    return <span style={{ color: "red" }}>{name}</span>;
+  }
+};
 
 /**
  * TEST 2
- * 
+ *
  * This is test is about handling changes of the data from an API.
- * 
+ *
  * Like test 1, this component has to return:
  *  The name in blue if the age is greater than or equal than 18.
  *  The name in red otherwise.
- * 
+ *
  * The difference is we don't have the age, we need to use the function below to get it.
  * The name from the parent can change any time, we have ensure the component rerenders if it happens
  * Getting data from an API (we simulate it with a timeout) is async, please be sure the code updates when we get the response back from the API
  */
-
 
 /**
  * This function accepts a name and simulates and API call to get the age of the person
@@ -48,25 +51,19 @@ export const Test1Component = (props: ITest1ComponentProps) => {
  * @param name Name of the person we want to find the age
  * @returns random integer from 0 to 39
  */
-const getAge = async (name: string): Promise<number>  => {
-    // This function calls an API and returns
-    return new Promise((resolve, reject) => {
-        return setTimeout(() => {
-            resolve(Math.floor(Math.random() * 40));
-        },500);
-    })
-}
+const getAge = async (name: string): Promise<number> => {
+  // This function calls an API and returns
+  return new Promise((resolve, reject) => {
+    return setTimeout(() => {
+      resolve(Math.floor(Math.random() * 40));
+    }, 500);
+  });
+};
 
 interface ITest2ComponentProps {
-    name: string;
+  name: string;
 }
 
 export const Test2Component = (props: ITest2ComponentProps) => {
-
-    useEffect(() => {
-    }, []);
-    
-}
-
-
-
+  useEffect(() => {}, []);
+};
